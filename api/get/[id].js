16a22b876d92c04509833c0aa7e9c6069975ae78
Eric Adamski <er.adamski@gmail.com>
2020-02-07 11:31:15 -0500

    Add cors

--- api/get/[id].js
+++ api/get/[id].js
@@ -6,6 +6,22 @@ Airtable.configure({ apiKey: process.env.AIRTABLE_API });
 let base;
 
 module.exports = (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", ["OPTIONS", "POST"]);
+  res.seHeader("Access-Control-Allow-Headers", [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization"
+  ]);
+
+  if (req.method === 'OPTIONS') {
+    return res.end()
+  } else if (req.method !== "POST") {
+    return res.status(405).end()
+  }
+
   if (!base) {
     base = Airtable.base("appOWPYNB2wLPeF8j");
   }
